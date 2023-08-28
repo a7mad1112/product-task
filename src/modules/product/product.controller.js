1b8712b3ab43848aaad6b62c33f985836761e1ad
a7mad1112 <ahmalawneh79@gmail.com>
2023-08-28 22:27:34 +0300

    get all products

--- src/modules/product/product.controller.js
+++ src/modules/product/product.controller.js
@@ -1,6 +1,19 @@
 import productModel from '../../../db/models/product.model.js';
 
 export const addProduct = async (req, res) => {
-  const product = await productModel.create(req.body);
-  return res.json({ msg: 'success', product });
+  try {
+    const product = await productModel.create(req.body);
+    return res.json({ msg: 'success', product });
+  } catch (err) {
+    return res.json({ msg: 'Error, Cannot Add Product', err });
+  }
+};
+
+export const getAll = async (req, res) => {
+  try {
+    const products = await productModel.find();
+    return res.json({ msg: 'success', products });
+  } catch (err) {
+    return res.json({ msg: 'Error, Cannot Retrieve Products', err });
+  }
 };
--- src/modules/product/product.router.js
+++ src/modules/product/product.router.js
@@ -2,5 +2,6 @@ import express from 'express';
 import * as productController from './product.controller.js';
 const app = express();
 app.post('/', productController.addProduct);
+app.get('/', productController.getAll);
 
 export default app;
